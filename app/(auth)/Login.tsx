7950c49ce20cb8c3eb803e2615e3be917c6bb452
Nicolas <njcaceres04@gmail.com>
2025-11-01 21:42:29 -0500

    Added simple navigation button

--- app/(auth)/Login.tsx
+++ app/(auth)/Login.tsx
@@ -1,10 +1,27 @@
+import { useRouter } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Login() {
+  const router = useRouter();
   return (
     <View>
-      <Text>Login</Text>
+      <TouchableOpacity onPress={() => router.push("/home")}>
+        <Text style={styles.loginButton}>Iniciar Sesión</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  loginButton: {
+    backgroundColor: "#FF7E33",
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "center",
+    marginVertical: 10,
+    alignSelf: "center",
+  },
+});
